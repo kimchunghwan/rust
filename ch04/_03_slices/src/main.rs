add8cf1c092427ec8543a8da36f39f5f23e4c3d6
kim chunghwan <kei2k84@gmail.com>
2022-05-05 07:45:49 +0900

    ch04_03

--- ch04/_03_slices/src/main.rs
+++ ch04/_03_slices/src/main.rs
@@ -21,4 +21,16 @@ fn main() {
 
     // word still has the value 5 here, but there's no more string that
     // we could meaningfully use the value 5 with. word is now totally invalid!
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = &my_string[0..6];
+    println!("first: {}", word);
+    let word = &my_string[..];
+    println!("second: {}", word);
+    println!("my_string: {}", my_string);
+
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2, 3]);
 }
